28501b3f30574630f7c6721d43961a047185bfeb
TeddyTjoe1990 <her21059@byui.edu>
2025-07-10 18:42:36 +0700

    update dependencies and adding databasehelper file

---
+++ app/lib/database_helper.dart
@@ -0,0 +1,51 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
+  static Database? _database;
+
+  DatabaseHelper._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('kasir.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String fileName) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, fileName);
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createDB,
+    );
+  }
+
+  Future _createDB(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE items (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        price REAL
+      )
+    ''');
+  }
+
+  Future<int> insertItem(String name, double price) async {
+    final db = await instance.database;
+    return await db.insert('items', {'name': name, 'price': price});
+  }
+
+  Future<List<Map<String, dynamic>>> getItems() async {
+    final db = await instance.database;
+    return await db.query('items');
+  }
+
+  Future close() async {
+    final db = await instance.database;
+    db.close();
+  }
+}
