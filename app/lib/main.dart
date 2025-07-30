e7c910f8d6d408c32fd527a69d1baf819df93c94
vickygiovi <vickygiovi@gmail.com>
2025-07-30 14:16:12 -0300

    PDF Receipt

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,92 +1,135 @@
 import 'package:flutter/material.dart';
+import 'package:app/services/invoice_service.dart';
+import 'package:app/model/product.dart';
 
-void main() => runApp(CashierApp());
+void main() {
+  runApp(const MyApp());
+}
 
-class CashierApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Cashier App',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: CashierHome(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
     );
   }
 }
 
-class CashierHome extends StatefulWidget {
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
   @override
-  _CashierHomeState createState() => _CashierHomeState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _CashierHomeState extends State<CashierHome> {
-  final itemNameController = TextEditingController();
-  final unitPriceController = TextEditingController();
-  final quantityController = TextEditingController();
-  final amountPaidController = TextEditingController();
-
-  int totalAmount = 0;
-  int changeAmount = 0;
-
-  void calculateTotal() {
-    final unitPrice = int.tryParse(unitPriceController.text) ?? 0;
-    final quantity = int.tryParse(quantityController.text) ?? 0;
-    final amountPaid = int.tryParse(amountPaidController.text) ?? 0;
-
-    final total = unitPrice * quantity;
-    final change = amountPaid - total;
-
-    setState(() {
-      totalAmount = total;
-      changeAmount = change;
-    });
-  }
+class _HomePageState extends State<HomePage> {
+  final PdfInvoiceService service = PdfInvoiceService();
+  List<Product> products = [
+    Product("Membership", 9.99, 19),
+    Product("Nails", 0.30, 19),
+    Product("Hammer", 26.43, 19),
+    Product("Hamburger", 5.99, 7),
+  ];
+  int number = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Cashier App')),
+      appBar: AppBar(
+        title: const Text("Invoice Generator"),
+      ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              TextField(
-                controller: itemNameController,
-                decoration: InputDecoration(labelText: 'Item Name'),
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  final currentProduct = products[index];
+                  return Row(
+                    children: [
+                      Expanded(child: Text(currentProduct.name)),
+                      Expanded(
+                        child: Column(
+                          children: [
+                            Text("Price: ${currentProduct.price.toStringAsFixed(2)} €"),
+                            Text("VAT ${currentProduct.vatInPercent.toStringAsFixed(0)} %")
+                          ],
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                        ),
+                      ),
+                      Expanded(
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: IconButton(
+                                onPressed: () {
+                                  setState(() => currentProduct.amount++);
+                                },
+                                icon: const Icon(Icons.add),
+                              ),
+                            ),
+                            Expanded(
+                              child: Text(
+                                currentProduct.amount.toString(),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                            Expanded(
+                              child: IconButton(
+                                onPressed: () {
+                                  setState(() => currentProduct.amount--);
+                                },
+                                icon: const Icon(Icons.remove),
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  );
+                },
+                itemCount: products.length,
               ),
-              TextField(
-                controller: unitPriceController,
-                decoration: InputDecoration(labelText: 'Unit Price'),
-                keyboardType: TextInputType.number,
-              ),
-              TextField(
-                controller: quantityController,
-                decoration: InputDecoration(labelText: 'Quantity'),
-                keyboardType: TextInputType.number,
-              ),
-              TextField(
-                controller: amountPaidController,
-                decoration: InputDecoration(labelText: 'Amount Paid'),
-                keyboardType: TextInputType.number,
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: calculateTotal,
-                child: Text('Calculate Total'),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Total: Rp$totalAmount',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              Text(
-                'Change: Rp$changeAmount',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [const Text("VAT"), Text("${getVat()} €")],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [const Text("Total"), Text("${getTotal()} €")],
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                final data = await service.createInvoice(products);
+                service.savePdfFile("invoice_$number", data);
+                number++;
+              },
+              child: const Text("Create Invoice"),
+            ),
+          ],
         ),
       ),
     );
   }
+
+  getTotal() => products
+      .fold(0.0, (double prev, element) => prev + (element.price * element.amount))
+      .toStringAsFixed(2);
+
+  getVat() => products
+      .fold(
+          0.0,
+          (double prev, element) =>
+              prev + (element.price / 100 * element.vatInPercent * element.amount))
+      .toStringAsFixed(2);
 }
